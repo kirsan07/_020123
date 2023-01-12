25978500c2c7fa93fc5515cc944a8cb3f3c40403
t0in4 <t0in4@yahoo.com>
2023-01-12 11:21:48 -0500

    решение с помощью регулярного выражения
    было решение только для числа 1. В условии задачи было дать решение для числа больше 0. 
    Немного упростил, программа проверяет только числа от 1 до 9

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
-        int one = scanner.nextInt();
-        int two = scanner.nextInt();
-        int three = scanner.nextInt();
-        if (one ==1 & two != 1 & three != 1){
+        // использовали next() чтобы записать ввод в одну строку
+        String one = scanner.next();
+        String two = scanner.next();
+        String three = scanner.next();
+        // для решения используем регулярное выражение
+        // указали диапазон с 1 до 9, т.к. было условие если ввод больше 0
+        String regex = "[1-9]"; 
+        // использовали сравнение matches()
+        if (one.matches(regex) & !two.matches(regex) & !three.matches(regex)){
             System.out.println("true");
         }
-        else if (one !=1 & two == 1 & three != 1){
+        else if (!one.matches(regex) & two.matches(regex) & !three.matches(regex)){
             System.out.println("true");
         }
-        else if (one !=1 & two != 1 & three == 1){
+        else if (!one.matches(regex) & !two.matches(regex) & three.matches(regex)){
             System.out.println("true");
         }
         else {
